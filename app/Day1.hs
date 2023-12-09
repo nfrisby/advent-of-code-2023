f747d3fa61a4e9d5afd7485b8d631b5957c5507a
Nicolas Frisby <nicolas.frisby@gmail.com>
2023-12-09 12:53:16 -0800

    puzzle 2

--- app/Day1.hs
+++ app/Day1.hs
@@ -1,18 +1,27 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiWayIf #-}
 
 module Day1 (main) where
 
+import           Control.Arrow ((&&&))
+import           Data.List (isPrefixOf)
 import qualified Data.Semigroup as Semi
 
 main :: IO ()
-main = interact $ show . sum . map decode . lines
+main = interact $ show . (part1 &&& part2) . lines
+
+part1 :: [String] -> Int
+part1 = sum . map decode
 
 decode :: String -> Int
 decode s =
     paste s
-        ( fmap Semi.getFirst $ pluck (Just . Semi.First) s
-        , fmap Semi.getLast  $ pluck (Just . Semi.Last ) s
+        ( f Semi.getFirst Semi.First
+        , f Semi.getLast  Semi.Last
         )
+  where
+    f :: Semigroup w => (w -> Int) -> (Int -> w) -> Maybe Int
+    f x y = fmap x $ pluck (Just . y) s
   
 paste :: String -> (Maybe Int, Maybe Int) -> Int
 paste s = \case
@@ -32,3 +41,32 @@ pluck inj = foldMap $ \case
     '8' -> inj 8
     '9' -> inj 9
     _   -> mempty
+
+-----
+
+part2 :: [String] -> Int
+part2 = sum . map (decode . bake)
+
+bake :: String -> String
+bake =
+    id
+  . expand ("zero" , '0')
+  . expand ("one"  , '1')
+  . expand ("two"  , '2')
+  . expand ("three", '3')
+  . expand ("four" , '4')
+  . expand ("five" , '5')
+  . expand ("six"  , '6')
+  . expand ("seven", '7')
+  . expand ("eight", '8')
+  . expand ("nine" , '9')
+
+expand :: (String, Char) -> String -> String
+expand (name, digit) =
+    go
+  where
+    go s = case s of
+      []   -> []
+      c:cs ->
+          (\x -> if name `isPrefixOf` s then name <> [digit] <> x else x)
+        $ c : go cs
