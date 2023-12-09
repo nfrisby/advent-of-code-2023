35c3eb14d8b58c662e13edd3fa77152d08d4db05
Nicolas Frisby <nicolas.frisby@gmail.com>
2023-12-09 13:28:59 -0800

    puzzle 4

--- app/Day2.hs
+++ app/Day2.hs
@@ -4,6 +4,7 @@
 
 module Day2 (main) where
 
+import           Control.Arrow ((&&&))
 import           Control.Monad (guard)
 import qualified Data.List.NonEmpty as NE
 import           Data.Maybe (mapMaybe)
@@ -13,7 +14,10 @@ import qualified Text.Megaparsec.Char as P
 import qualified Text.Megaparsec.Char.Lexer as L
 
 main :: IO ()
-main = interact $ show . sum . mapMaybe possible . map parse . lines
+main = interact $ show . (part1 &&& part2) . map parse . lines
+
+part1 :: [Game] -> Int
+part1 = sum . mapMaybe possible
 
 data Game = Game !Int (NE.NonEmpty (Pull Int))
 
@@ -21,9 +25,9 @@ data Pull a = Pull a a a
   deriving (Foldable, Functor)
 
 instance Applicative Pull where
-  pure x = Pull x x x
+    pure x = Pull x x x
 
-  Pull f1 f2 f3 <*> Pull x1 x2 x3 = Pull (f1 x1) (f2 x2) (f3 x3)
+    Pull f1 f2 f3 <*> Pull x1 x2 x3 = Pull (f1 x1) (f2 x2) (f3 x3)
 
 hypothesis :: Pull Int
 hypothesis = Pull 12 13 14
@@ -41,8 +45,8 @@ type P = P.Parsec Void String
 parse :: String -> Game
 parse =
     \s -> case P.runParser game "stdin" s of
-      Left e  -> error $ show e
-      Right g -> g
+        Left e  -> error $ show e
+        Right g -> g
   where
     game :: P Game
     game = do
@@ -63,10 +67,24 @@ parse =
 
     pull1 :: P (Pull Int)
     pull1 = do
-      count <- L.decimal
-      P.space
-      P.many P.letterChar >>= \case
-          "red"   -> pure $ Pull count 0 0
-          "green" -> pure $ Pull 0 count 0
-          "blue"  -> pure $ Pull 0 0 count
-          o       -> fail $ "bad color: " <> o
+        count <- L.decimal
+        P.space
+        P.many P.letterChar >>= \case
+            "red"   -> pure $ Pull count 0 0
+            "green" -> pure $ Pull 0 count 0
+            "blue"  -> pure $ Pull 0 0 count
+            o       -> fail $ "bad color: " <> o
+
+-----
+
+part2 :: [Game] -> Int
+part2 = sum . map gamePower
+
+gamePower :: Game -> Int
+gamePower (Game _ident pulls) =
+    power
+  $ let p NE.:| ps = pulls
+    in foldr (\x y -> max <$> x <*> y) p ps
+
+power :: Num a => Pull a -> a
+power (Pull a b c) = a * b * c
