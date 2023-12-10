a0f2d044d54b8bacb453f0aea23cdf52e212acc8
Nicolas Frisby <nicolas.frisby@gmail.com>
2023-12-10 08:36:36 -0800

    puzzle 18

--- app/Day9.hs
+++ app/Day9.hs
@@ -9,16 +9,18 @@ parse :: String -> [[Int]]
 parse = map (map read . words) . lines
 
 puzzle1 :: [[Int]] -> Int
-puzzle1 = sum . map predict
+puzzle1 = sum . map (fst . predict)
 
-predict :: [Int] -> Int
+predict :: [Int] -> (Int, Int)
 predict =
     go
   where
     go sq =
         if all (==0) sq
-        then 0
-        else last sq + go (diff sq)
+        then (0, 0)
+        else ( head sq - fst (go (diff sq))
+             , last sq + snd (go (diff sq))
+             )
 
 diff :: [Int] -> [Int]
 diff x = zipWith (-) (tail x) (init x)
@@ -26,4 +28,4 @@ diff x = zipWith (-) (tail x) (init x)
 -----
 
 puzzle2 :: [[Int]] -> Int
-puzzle2 = puzzle1
+puzzle2 = sum . map (snd . predict)
